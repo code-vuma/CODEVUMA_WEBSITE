e86668de6b8d9ce44cbba013b86b55fae320f75e
Tom Vuma <tom.vuma@khmtechnology.com>
2026-07-04 14:26:34 +0200

    Add error boundary to diagnose production crash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,37 @@
+import { Component } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null }
+  }
+  static getDerivedStateFromError(error) {
+    return { error }
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <pre style={{ padding: '2rem', color: 'red', whiteSpace: 'pre-wrap', wordBreak: 'break-all' }}>
+          {String(this.state.error)}
+          {'\n\n'}
+          {this.state.error?.stack}
+        </pre>
+      )
+    }
+    return this.props.children
+  }
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ErrorBoundary>
   )
 }
 
-export default App;
+export default App
